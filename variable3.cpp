607bbae2704b725c7d83411f2f686061ff24a13a
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:15:24 -0800

    local variable

---
+++ variable3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // local variable
+    int a,b;
+    int c;
+//initialization
+    cout<<"enter the first integer \n";
+    cin>>a;
+    cout<<"enter the second integer \n";
+    cin>>b;
+    
+    // mthamatical operation
+    c= b-a;
+    cout<<"Subtraction "<< c;
+    
+    
+
+    return 0;
+}
