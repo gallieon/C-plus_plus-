5eaa18bfadf54afca1110345c7b25cb40a02e789
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:51:26 -0800

    for loop

---
+++ for.cpp
@@ -0,0 +1,17 @@
+//forloop
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the loop time:\n";
+    cin>>b;
+    
+    for(a=0;a<b;a++)
+    {
+        cout<<"hello world:\n";
+    }
+}
+
+
