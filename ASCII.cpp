3d43d5e04a14fad4dd1f9b751ff514f2d0f71c37
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:53:29 -0800

    ascii value

---
+++ ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    char ch;
+    cout<<"enter a alphabet:\n";
+    cin>>ch;
+    cout<<"ASCII Value of "<<ch<<" is: "<<int(ch);
+
+    
+}
