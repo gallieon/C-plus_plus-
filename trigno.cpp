430c4df9a1f29fa0edc3d046cefa79f90c166d3b
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:57:34 -0800

    trignomatric ratio

---
+++ trigno.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a; // at the place of in you can also take "double"
+    cout<<"enter the angle value of sin() :\n ";
+    cin>>a;
+    std::cout<<"the value of the angle is:\n"<< sin(a);
+    
+}    
+
