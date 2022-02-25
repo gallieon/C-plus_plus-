bdf79b18a793853275658a66d0bee83f82d67bf8
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:18:22 -0800

    const keyword

---
+++ const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int length=10;
+    const int width=5;
+    const char newline='\n';
+    int area;
+    
+    area= length * width;
+    cout<<area;
+    cout<<newline;
+    return 0;
+    
+}
