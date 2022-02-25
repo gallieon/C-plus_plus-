069fc91c728db6e4ac8b800b33df96a64208479b
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:54:47 -0800

    array

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0,marks[5];
+    marks[0]=20;
+    marks[1]=30;
+    marks[2]=40;
+    marks[3]=50;
+    marks[4]=60;
+    marks[5]=70;
+    for(i=0;i<6;i++)
+    {
+    cout<<"\n"<<marks[i];
+    }
+     
+}
