939186c05afd38b509a698bb023ddc5d6d4a4a4e
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:52:43 -0800

    Odd or even

---
+++ identify.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    cout<<"enter the integer:\n";
+    cin>>a;
+    if(a % 2==0)
+    {
+        cout<<"this is an even number.";
+    }
+    else{
+        cout<<"this is an odd number.";
+    }
+    
+}
