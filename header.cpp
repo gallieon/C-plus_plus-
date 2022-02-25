3896e50796665c6712795b524d833ebc6ec08df3
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:49:34 -0800

    all header files of c++

---
+++ header.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include <string>
+// #include<math.h>
+// #include<iomanip.h>
+// #include<signal.h>
+// #include<stdarg.h>
+// #include<errno.h>
+// #include<fstream.h>
+// #include<time.h>
+// #include<float.h>
+// #include<limits.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
