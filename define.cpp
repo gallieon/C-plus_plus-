cf7f4271b66adec5fa1d1355ec59f7e71f0caddf
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:17:03 -0800

    preprocessor #define

---
+++ define.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+ using namespace std;
+ 
+ #define L 10
+ #define B 5
+ #define newline '\n'
+ 
+ int main()
+ {
+     int area;
+     
+     area=L*B;
+     cout<< area;
+     cout<<newline;
+     return 0;
+     
+ }
