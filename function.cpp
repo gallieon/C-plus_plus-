ab319bb1971c04174c96f03790a3d96b1c90494a
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:06:59 -0800

    function

---
+++ function.cpp
@@ -0,0 +1,88 @@
+// funtion without return type and argument...............................................................................................
+
+//example=1
+
+// #include<stdio.h>
+// void printname();
+// void main()
+// {
+//     printf("hello c\t");
+//     printname();
+// }
+// void printname()
+// {
+//     printf("how are you???????");
+// }
+
+//example=2
+
+// #include <stdio.h>
+// void sum();
+// void main()
+// {
+//     printf("here we are going to calcuate the sum of two numbers\n");
+//     sum();
+// }
+// void sum()
+// {
+//     int a,b;
+//     printf("enter first integer\n");
+//     scanf("%d",&a);
+                                                                
+//     printf("enter second integer\n");
+//     scanf("%d",&b);
+    
+//     printf("addition of the enters two integer is:%d",a+b);           // error is there.............
+    
+    
+// }
+
+
+
+// function with return value and without argumant......................................................................................
+
+// example=1 
+ 
+// #include <stdio.h>
+// int sum();
+// void main()
+// {
+//     int result;
+//     printf("\n here we are going to add two interd which is enterd by the user\n");
+//     result=sum();
+//     printf("the sum is : %d",result);
+// }
+// int sum()
+// {
+//     int a,b;
+//     printf("enter the first number\n");
+//     scanf("%d",&a);
+    
+//     printf("enter the first number\n");
+//     scanf("%d",&b);
+    
+//     return a+b;
+// }
+
+//example=2
+
+// #include <stdio.h>
+// int sum();
+// void main()
+// {
+//     float rectangle;
+//     printf("here we are going to  calculate the are of the rectangle as per given perameters\n");
+//     rectangle=area();
+//     printf("the area of the ractangle is :%f",rectangle);
+    
+// }
+// int area()
+// {
+//     float a,b;
+//     printf("enter the length of the rectangle:\n");
+//     scanf("%f",&a);
+//     printf("enter the breadth of the rectangle:\n");
+//     scanf("%f",&b);
+
+//     return a*b;    
+// }
