0f5a35fc6b2fadd2e19c0dbdea33bbd710ebfb4b
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:23:31 -0800

    do while loop

---
+++ loop1.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+     int a=2;
+      do{
+          cout<<"value of a: "<<a<<endl;
+          a = a + 2;
+          
+      }
+      while(a<=20);
+ }
