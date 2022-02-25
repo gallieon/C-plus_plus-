6d2551eb7fb82a2436f460e1445a21028f090769
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:50:56 -0800

    infinite loop

---
+++ infinite.cpp
@@ -0,0 +1,16 @@
+//forloop
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the loop time:\n";
+    cin>>b;
+    
+    for(a=0;a<b;a++)
+    {
+        cout<<"hello world:\n";
+    }
+}
+
