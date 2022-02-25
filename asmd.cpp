479d7933bab16780eddd984839b8565d8bc4650a
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:48:44 -0800

    add,sub,mul,div in one

---
+++ asmd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int add,sub,mul,divi,b, a;
+    cout <<"enter first integer:\n";
+    cin>> a;
+    cout<<"enter second integer:\n";
+    cin >> b;
+    add=a+b;
+    sub=a-b;
+    mul=a*b;
+    divi=a/b;
+    cout<<"the addition is: "<<add;
+    cout<<"\nthe subtraction is:"<<sub;
+    cout<<"\nthe multiplication is: "<<mul;
+    cout<<"\nthe division is: "<<divi;
+    
+}    
