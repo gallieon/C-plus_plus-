ddc59407af669338f820de1f116931066e876b5d
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-24 23:47:43 -0800

    Add two number

---
+++ Add.cpp
@@ -0,0 +1,12 @@
+  #include <iostream>
+ using namespace std;
+ int main()
+ {
+     int a,b;
+     cout<<"enter two intergers:--"<<endl;
+     cin>>a>>b;
+     cout<<"first number is: "<<a<<"\nsecond number is: "<<b;
+     
+     
+ }
+ 
