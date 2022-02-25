97fa9b54a6180da83ab1b69534ddb4a65172bf9a
gallieon <95416064+gallieon@users.noreply.github.com>
2022-02-25 00:23:00 -0800

    while loop

---
+++ loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=0;
+    //locl variable decliration
+    
+    //while loop
+    while(a<=20)        
+    {
+        cout<<"value of a: "<<a<<endl;
+        a++;
+    }
+    
+}
